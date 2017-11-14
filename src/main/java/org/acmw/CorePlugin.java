4c905fd5a4be8d9c7d27a56043e4444b6733eccf
Mose <Mose@192.168.1.106>
2017-11-14 21:44:51 +0000

    Maven setup

---
+++ src/main/java/org/acmw/CorePlugin.java
@@ -0,0 +1,14 @@
+package org.acmw;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CorePlugin extends JavaPlugin {
+	
+	CorePlugin plugin;
+	
+	public void onEnable() {
+		plugin = this;
+ 		 
+	}
+
+}
